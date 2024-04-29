2159333e3e8f4a205da0200c01a4affe860eaf60
Jose Alcocer <jolual2747@gmail.com>
2024-04-28 23:54:30 -0500

    fixing embeddings bugs

--- src/products_app/app.py
+++ src/products_app/app.py
@@ -1,7 +1,18 @@
+import PIL.Image
 import streamlit as st
-from utils import get_client
 import PIL
+import numpy as np
+from io import BytesIO
+from io import StringIO
+import torch
+from utils import (
+    get_client, 
+    plot_images_horizontal, 
+    load_model, 
+    get_products_data
+)
 
+collection = "products2"
 
 def main() -> None:
     """
@@ -10,14 +21,49 @@ def main() -> None:
     st.title("Search a product by an image as query.")
     st.write("Upload your product!")
     client = get_client("localhost", 6333)
+    processor, model = load_model()
+    ds_pandas = get_products_data()
 
     with st.sidebar:
         st.title("Upload your product!")
-        uploaded_file = st.file_uploader("Upload a pic", type=["png"])
+        uploaded_file = st.file_uploader("Upload a pic", type=["png", "jpeg"])
         if uploaded_file and st.button("Search"):
             st.image(uploaded_file)
-            st.write(type(uploaded_file))
+            st.session_state.uploaded_img = PIL.Image.open(uploaded_file)
+            st.write(PIL.Image.open(uploaded_file).mode)
 
+    if uploaded_file and "uploaded_img" in st.session_state:
+        inputs = processor(images=st.session_state.uploaded_img, return_tensors="pt")
+        with torch.no_grad():
+            outputs = model(**inputs)
+            embedding = outputs.pooler_output
+        st.write(len(embedding.detach().cpu().numpy().tolist()[0]))
+        st.write(embedding.mean())
+        st.write(f"Pixels mean {np.array(st.session_state.uploaded_img).mean()}")
+        st.write(f'pixel values mean: {inputs["pixel_values"].mean()}')
+    
+        results = client.search(
+            collection_name=collection,
+            query_vector=embedding.detach().cpu().numpy().tolist()[0],
+            score_threshold = 0.001,
+            limit=10
+        )
+        res = [result.id for result in results]
+        descriptions = [result.payload["title"] for result in results]
+        similar_images = []
+
+        st.write(res)
+
+        for row in ds_pandas.iloc[res,]["image"]:
+            similar_images.append(np.array(PIL.Image.open(BytesIO(row["bytes"]))))
+
+        st.write(similar_images)
+        st.write(results)
+
+        
+        plot_images_horizontal(similar_images, titles=descriptions)
+            
+        
 
 if __name__ == '__main__':
     main()
--- src/products_app/utils.py
+++ src/products_app/utils.py
@@ -2,6 +2,11 @@ from qdrant_client import QdrantClient
 from transformers import ViTImageProcessor, ViTModel
 from typing import Tuple
 import streamlit as st
+import matplotlib.pyplot as plt
+from typing import List
+import numpy as np
+import pandas as pd
+from ast import literal_eval
 
 def get_client(host: str, port: int) -> QdrantClient:
     """Get a QdrantClient to manage vector databases. 
@@ -25,4 +30,45 @@ def load_model() -> Tuple[ViTImageProcessor, ViTModel]:
     model_id = 'jolual2747/vit-clothes-classification'
     processor = ViTImageProcessor.from_pretrained(model_id)
     model = ViTModel.from_pretrained(model_id)
-    return model, processor
+    return processor, model
+
+@st.cache_data
+def get_products_data() -> pd.DataFrame:
+    """Load products data.
+
+    Returns:
+        pd.DataFrame: Products with metadata and images.
+    """
+    df = pd.read_csv("./data/structured/products.csv")
+    df["image"] = df["image"].apply(literal_eval)
+    return df
+
+def plot_images_horizontal(image_list: List[np.ndarray], titles: List[str] = None) -> None:
+    """Plot images from a list of arrays.
+
+    Args:
+        image_list (List[np.ndarray]): List of images as arrays.
+        titles (List[str], optional): Titles to map to every image. Defaults to None.
+    """
+    num_images = len(image_list)
+
+    if num_images != len(titles):
+        raise ValueError(f"Number of images is different from number of titles")
+    
+    # Create fig and axes
+    fig, axes = plt.subplots(1, num_images, figsize=(num_images * 4, 4))
+    
+    # Hide axis
+    for ax in axes:
+        ax.axis('off')
+    
+    # Plot images
+    for i, (image, ax) in enumerate(zip(image_list, axes)):
+        ax.imshow(image)  
+        if titles is not None:
+            ax.set_title(f"\n{titles[i]}")    
+
+    fig.suptitle(f'Similar {num_images} products', fontsize=20)
+    plt.tight_layout()
+
+    st.pyplot(fig)
