8da1118b65aba90a6934d4255e164019a487a2fb
Jose Alcocer <jolual2747@gmail.com>
2024-05-11 19:34:39 -0500

    added metadata to uploaded songs, and display results

--- src/songs_app/app.py
+++ src/songs_app/app.py
@@ -1,14 +1,16 @@
-import PIL.Image
+import os
+import pandas as pd
 import streamlit as st
-import PIL
-import numpy as np
-from io import BytesIO
-import torch
-import time
+import streamlit.components.v1 as components
+from IPython.display import Audio
 from utils import (
     extract_audio_from_youtube_video,
     get_client,
-    tmp_folder
+    tmp_folder,
+    load_model,
+    read_audio_as_array,
+    get_one_embedding,
+    get_youtube_video_info
 )
 
 collection = "music_collection3"
@@ -17,20 +19,77 @@ def main() -> None:
     """
     Main of the Streamlit app. 
     """
-    st.title("Find cool songs.")
+    # Title and resources
+    st.set_page_config(layout="wide")
+    st.title("Find Cool Songs. 🎵🎶😎")
     st.write("Enter a URL YouTube video and find songs similar to your favorite ones")
+    model = load_model()
+    client = get_client("localhost", 6333)
 
+    # Input for URL
     with st.sidebar:
         st.title("Upload your URL!")
         url = st.text_input("Copy and paste an URL from YouTube")
         if url and st.button("Search"):
             st.session_state.url = url
+            uploaded_video, thumbnail = get_youtube_video_info(st.session_state.url)
+            st.subheader(f"Uploaded video ✅:\n{uploaded_video}")
+            st.image(thumbnail)
 
-        
+    # Download video, embedding and search similar songs
     if url and "url" in st.session_state:
         with st.spinner("Please wait, downloading video..."):
             extract_audio_from_youtube_video(url, "audio.mp3")
 
+        # Compute embedding
+        frame_rate, audio_array = read_audio_as_array(os.path.join(tmp_folder, "audio.mp3"))
+        embedding = get_one_embedding(model, audio_array)
+        embedding = embedding[0].tolist()
+
+        # Search similar songs
+        results = client.search(
+            collection_name=collection,
+            query_vector=embedding,
+            limit=10
+        )
+
+        # Display results
+        if len(results) > 0:
+            st.markdown("---")
+            songs_names = [result.payload for result in results]
+            for idx, song in enumerate(songs_names):
+                song["similarity"] = round(results[idx].score, 2)               
+
+            st.markdown("""## Results:""")            
+
+            row1 = st.columns(2)
+            row2 = st.columns(2)
+            row3 = st.columns(2)
+            row4 = st.columns(2)
+            row5 = st.columns(2)
+
+            for idx, col in enumerate(row1 + row2 + row3 + row4 + row5):
+                tile = col.container(height=220)
+                tile.write(f"Result {idx + 1}")
+                tile.dataframe(pd.DataFrame.from_records([songs_names[idx]]).drop(columns = ["urls"]), hide_index = True)
+                tile.audio(songs_names[idx]["urls"][1:])
+
+            # res = [result.id for result in results]
+            # songs_names = [result.payload for result in results]
+            # entries_to_remove = ('subgenres', 'urls')
+            # for k in entries_to_remove:
+            #     for song in songs_names:
+            #         song.pop(k, None)
+            # st.write(songs_names)
+            # for song in songs_names:
+            #     st.dataframe(pd.DataFrame.from_records([song]).drop(columns = ["urls"]))
+            #     st.audio(song["urls"][1:])
+
+            # data = pd.DataFrame.from_records(songs_names)
+            # st.dataframe(data)
+        
+        else:
+            st.error("Not found similar songs. Try with another!")
 
 if __name__ == '__main__':
     main()
--- src/songs_app/utils.py
+++ src/songs_app/utils.py
@@ -1,7 +1,17 @@
 import os
+import PIL.Image
+import numpy as np
+import torch
+import PIL
+from io import BytesIO
+from typing import Any, Tuple, Optional
 from pytube import YouTube
+import requests
+from bs4 import BeautifulSoup
 from pydub import AudioSegment
 from qdrant_client import QdrantClient
+from panns_inference import AudioTagging
+import streamlit as st
 
 tmp_folder = os.path.join(os.getcwd(), "src", "songs_app", "tmp")
 
@@ -45,3 +55,81 @@ def get_client(host: str, port: int) -> QdrantClient:
         QdrantClient: QdrantClient
     """
     return QdrantClient(host=host, port=port)
+
+@st.cache_resource
+def load_model() -> Any:
+    """Load PANNs model embedding.
+
+    Returns:
+        Any: Model embedding.
+    """
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    at = AudioTagging(checkpoint_path=None, device=device)
+    return at
+
+def read_audio_as_array(file_path: str, normalized: bool = True) -> Tuple[int, np.ndarray]:
+    """MP3 to numpy array
+
+    Args:
+        file_path (str): Path to .mp3 file.
+        normalized (bool, optional): To normalize data. Defaults to True.
+
+    Returns:
+        Tuple[int, np.ndarray]: Frame rate and audio array.
+    """
+    a = AudioSegment.from_mp3(file_path)
+    y = np.array(a.get_array_of_samples())
+    if a.channels == 2:
+        y = y.reshape((-1, 2))
+    if normalized:
+        return a.frame_rate, np.float32(y) / 2**15
+    else:
+        return a.frame_rate, y    
+
+def get_one_embedding(model: Any, audio_array: np.ndarray) -> np.ndarray:
+    """Generate embeddings from an audio array.
+
+    Args:
+        model (Any): PANNs model embedding.
+        audio_array (np.ndarray): Numpy array representation of audio .mp3 file.
+
+    Returns:
+        np.ndarray: Embedding.
+    """
+    array = torch.tensor(audio_array, dtype=torch.float64).unsqueeze(0)
+    inputs = torch.nn.utils.rnn.pad_sequence(array, batch_first=True, padding_value=0).type(torch.FloatTensor)
+    with torch.no_grad():
+        _, embedding = model.inference(inputs)
+    return embedding
+
+def get_youtube_video_info(url: str) -> Tuple[str, Optional[PIL.Image.Image]]:
+    """Get YouTube's video title and thumbnail.
+
+    Args:
+        url (str): URL of Youtube Video. 
+
+    Returns:
+        Tuple[str, Optional[PIL.Image.Image]]: Title and thumbnail.
+    """
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+
+    link = soup.find_all(name="title")[0]
+    title = link.text
+
+    # Find og:image metadata
+    meta_image = soup.find('meta', property='og:image')
+
+    # Get thumbnail image
+    thumbnail = None
+    if meta_image:
+        thumbnail_url = meta_image['content']
+        print("Miniatura del video:", thumbnail_url)
+
+        # Download image
+        response_image = requests.get(thumbnail_url)
+
+        # Convert to PIL object
+        thumbnail = PIL.Image.open(BytesIO(response_image.content))
+
+    return title, thumbnail
