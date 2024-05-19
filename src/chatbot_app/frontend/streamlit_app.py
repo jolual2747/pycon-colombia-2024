dd1817b21a7711096259f9d85254264b9923c9fe
Jose Alcocer <jolual2747@gmail.com>
2024-05-18 20:05:47 -0500

    added header to chatbot frontend and add pyconco info

--- src/chatbot_app/frontend/streamlit_app.py
+++ src/chatbot_app/frontend/streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import requests
 import time
-from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
-#from langchain.callbacks import StreamlitCallbackHandler
-from langchain.callbacks.base import BaseCallbackHandler
-from langchain.schema import LLMResult
-from typing import Any
+import base64
 
-company_name = "Tuya S.A"
+company_name = "PyCon Colombia 2024"
+
+def draw_something_on_top_of_page_navigation():
+    st.sidebar.markdown(
+        "My Logo (sidebar) should be on top of the Navigation within the sidebar"
+    )
 
 def start_over_with_new_document() -> None:
     """
@@ -25,39 +26,44 @@ def main() -> None:
     """
     Main of the Streamlit app. 
     """
-    st.title("Chat with your documents")
-    st.write("Upload your document and start chatting!")
+    st.image("pyconco2024.png")
+    st.markdown("# PyCon Colombia 2024 😎")
+    st.markdown("### Non-official Chabot 🤖")
+    st.write("Ask me anything about the event!")
+    
+    if st.button("Start chat!"):
+        st.session_state.start = True
 
+    if "start" in st.session_state:
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+            welcome_message = f"I am an AI chatbot from {company_name} ready to help you to answer any questions!"
+            st.session_state.messages.append({"role": "assistant", "content": welcome_message})
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+    
+        prompt = st.chat_input('Ask one or more questions about the content of the uploaded data:', key="text_input")
+        if prompt:
+            st.chat_message("user").markdown(prompt)
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-        welcome_message = f"I am an AI chatbot from {company_name} ready to help you to answer any questions!"
-        st.session_state.messages.append({"role": "assistant", "content": welcome_message})
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+            with st.chat_message("assistant"):
+                text_container = st.empty()
+                full_text = ""
 
-    prompt = st.chat_input('Ask one or more questions about the content of the uploaded data:', key="text_input")
-    if prompt:
-        st.chat_message("user").markdown(prompt)
-        st.session_state.messages.append({"role": "user", "content": prompt})
+                s = requests.Session()
+                with s.post("http://localhost:8000/chat_stream", stream=True, json={"text": prompt}) as r:
+                    for line in r.iter_content():
+                        full_text += line.decode("latin-1").replace('`', '') 
+                        if full_text.startswith('"'):
+                            full_text = full_text[1:]
 
-        with st.chat_message("assistant"):
-            text_container = st.empty()
-            full_text = ""
-
-            s = requests.Session()
-            with s.post("http://localhost:8000/chat_stream", stream=True, json={"text": prompt}) as r:
-                for line in r.iter_content():
-                    full_text += line.decode("latin-1").replace('`', '') 
-                    if full_text.startswith('"'):
-                        full_text = full_text[1:]
-
-                    if full_text.endswith('"') or full_text.endswith('}') or full_text.endswith('`'):
-                        full_text = full_text[:-1]
-                    text_container.write(full_text) 
-                    time.sleep(0.005)
-        st.session_state.messages.append({"role": "assistant", "content": full_text})
+                        if full_text.endswith('"') or full_text.endswith('}') or full_text.endswith('`'):
+                            full_text = full_text[:-1]
+                        text_container.write(full_text) 
+                        time.sleep(0.005)
+            st.session_state.messages.append({"role": "assistant", "content": full_text})
 
     # if st.session_state.text_input:
     #     st.button('Start again from new context', key='new_question_new_context')
